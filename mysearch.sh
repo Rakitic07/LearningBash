567ce910020650113e11ba9aa4986c74bfcae476
Raktim Biswas <biswasra@in.ibm.com>
2017-12-13 11:34:53 +0530

    2nd commit

--- mysearch.sh
+++ mysearch.sh
@@ -8,10 +8,20 @@
 #res=$(grep -Rl "Abhilash\|Raktim" .)
 #echo "The matched strings were found in these Files: "  $res
 
-if grep -RL "Raktim\|Abhilash" .
-then
-	sed -i "s/Raktim/Subbu/g;s/Abhilash/Sukanta/g" *
-fi
+#grep -rL "Raktim\|Abhilash" ./ | sed -e "s/Raktim/Subbu/g;s/Abhilash/Sukanta/g"
+
+#git grep -l "Raktim\|Abhilash" ./ | xargs sed -i "s/Raktim/Subbu/g;s/Abhilash/Sukanta/g"
+
+#replace "Raktim\|Abhilash" "Subbu\|Sukanta" ./        --For MySQL
+
+
+
+#find ./ -name .git -prune -o -type f -exec sed -i.bak -e "s/Raktim/Subbu/g;s/Abhilash/Sukanta/g" {} \;     --.bak to take the backup of the original file and make the changes henceforth.
+find ./ -name .git -prune -o -type f -exec sed -i -e "s/Raktim/Subbu/g;s/Abhilash/Sukanta/g" {} \;
+
+
+
+
 #else
 #echo "Not found"
 #fi
